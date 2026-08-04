cc9eadb400f73f0ecdd85da044adcb7ae8f3415e
Lýz Lissade <147095077+Lyzurly@users.noreply.github.com>
2026-08-03 22:38:02 -0500

    bridgestuff

--- Scripts/bridge.gd
+++ Scripts/bridge.gd
@@ -1,21 +1,27 @@
 class_name Bridge extends Node3D
 
-@onready var _bridge_model: MeshInstance3D = %bridge_center_wood
-@onready var _collision: CollisionShape3D = %CollisionShape3D
+static var _bridge_model: MeshInstance3D
+static var _collision: CollisionShape3D
 
 @export var bridge_number: int
 
-static var _built: bool = false
+func _ready() -> void:
+	%BridgeManager.build_bridge.connect(_on_build)
+	_bridge_model = %bridge_center_wood
+	_collision = %CollisionShape3D
 
-static func build(
+var _built: bool = false
+
+func _on_build(
 coins:int,bridges_built:int
-) -> bool:
+) -> void:
+	print("trying to build bridge ",bridge_number,"...")
 	if coins > 0 \
 	and not _built \
 	and bridges_built+1 == bridge_number:
+		print("building bridge ",bridge_number," because bridges built are ",bridges_built)
 		_bridge_model.show()
-		_collision.disable = true
+		_collision.disabled = true
 		_built = true
-		return true
-	return false
+		Player.bridges_built +=1
 		
--- Scripts/bridge_manager.gd
+++ Scripts/bridge_manager.gd
@@ -1,12 +1,11 @@
 extends Node
 
-@onready var _bridge_1: Node3D = %bridge1
-@onready var _bridge_2: Node3D = %bridge2
-@onready var _bridge_3: Node3D = %bridge3
-
-
+signal build_bridge(coins_collected:int,bridges_build:int)
 func _on_point_of_interest_player_entered() -> void:
-	pass # Replace with function body.
+	print("trybuild bridge")
+	build_bridge.emit(
+		Player.coins_collected,
+		Player.bridges_built)
 
 
 func _on_point_of_interest_player_exited() -> void:
